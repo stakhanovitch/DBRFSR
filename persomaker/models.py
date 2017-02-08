b3af9f91897090247364032f40bc3b3c85c967a3
STKHVTCH <valentin.reverdy@gmail.com>
2017-02-08 21:15:32 +0100

    UPDATE creation : view + template

--- persomaker/models.py
+++ persomaker/models.py
@@ -3,14 +3,62 @@ from django.db import models
 
 
 class Character(models.Model):
-    # picture = models.ImageField(null=True)
+    ########### RP ###########
+    picture = models.ImageField(null=True)
     name = models.CharField(max_length=70)
     # alias = models.CharField(max_length=70)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
     ethnicity = models.CharField(max_length=70)
+    age = models.IntegerField(default=0)
+    MALE = 'M'
+    FEMALE = 'F'
+    UNKNOWN = 'U'
+    SEX_CHOICE = ((MALE, 'Male'),
+                  (FEMALE, 'Female'),
+                  (UNKNOWN, 'Unknown'),
+                  )
+    sex = models.CharField(
+        max_length=1,
+        choices= SEX_CHOICE,
+        default=UNKNOWN,
+        )
+        
+    ########### Stat ###########
     karma = models.IntegerField(default=0)
     nuyen = models.IntegerField(default=0)
+    credibility = models.IntegerField(default=0)
+    notoriety = models.IntegerField(default=0)
+    awareness = models.IntegerField(default=0)
+
+    ########### Skills  ###########
     skills = models.ManyToManyField('Skill', through='CharacterSkill', related_name='characters')
+
+    ###########  calculated skills  ###########
+    initiative_physical = models.IntegerField(default=0)
+    initiative_ar = models.IntegerField(default=0)    
+    initiative_coldsim = models.IntegerField(default=0)
+    initiative_hotsim = models.IntegerField(default=0)
+    initiative_physical = models.IntegerField(default=0)
+    initiative_astral = models.IntegerField(default=0)
+    
+    limit_mental = models.IntegerField(default=0)
+    limit_physical = models.IntegerField(default=0)
+    limit_social = models.IntegerField(default=0)
+
+    condition_physical = models.IntegerField(default=0)
+    condition_stun = models.IntegerField(default=0)
+    condition_overflow = models.IntegerField(default=0)
+    
+    living_personna_attack = models.IntegerField(default=0)
+    living_personna_dataprocessing = models.IntegerField(default=0)
+    living_personna_devicerating = models.IntegerField(default=0)
+    living_personna_firewall = models.IntegerField(default=0)
+    living_personna_sleeze = models.IntegerField(default=0)
+
+    attribute_skill_composure = models.IntegerField(default=0)
+    attribute_skill_judgeintention = models.IntegerField(default=0)
+    attribute_skill_lifting = models.IntegerField(default=0)
+    attribute_skill_memory = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
@@ -37,6 +85,7 @@ class CharacterSkill(models.Model):
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     level = models.IntegerField(default=0)
     levelmax = models.IntegerField(default=0)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
         return "%s, %s" % (self.character, self.skill)
--- persomaker/views.py
+++ persomaker/views.py
@@ -5,15 +5,15 @@ from itertools import chain
 from .models import Character,Skill,CharacterSkill
 from django.urls import reverse
 from django.views import generic
-
+from math import ceil
 
 class IndexView(generic.ListView):
     template_name = 'persomaker/index.html'
-    context_object_name = 'latest_character_list'
+    context_object_name = 'character_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Character.objects.order_by('-pub_date')[:5]
+        return Character.objects.order_by('-name')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -27,15 +27,53 @@ class ResultsView(generic.DetailView):
 
 
 def creation(request, pk):
+    def final_calculation(character):
+        character.initiative_physical = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Reaction').id).level)
+        character.initiative_ar = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Reaction').id).level)
+        #Character.initiative_coldsim =  
+        #Character.initiative_hotsim = 
+        character.initiative_astral = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)*2
+
+        character.limit_mental = ceil((int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Logic').id).level)*2+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level))/3)
+        character.limit_physical = ceil((int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Strength').id).level)*2+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Body').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Reaction').id).level))/3) 
+        character.limit_social =ceil((int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Charisma').id).level)*2+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Essence').id).level))/3) 
+
+        character.condition_physical = ceil(int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Body').id).level)/2)+8
+        character.condition_stun = ceil(int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level)/2)+8
+        character.condition_overflow = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Body').id).level)
+
+        character.living_personna_attack = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Charisma').id).level) 
+        character.living_personna_dataprocessing = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Logic').id).level) 
+        character.living_personna_devicerating = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Resonance').id).level) 
+        character.living_personna_firewall = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level) 
+        character.living_personna_sleeze = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)
+        character.attribute_skill_composure = int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Charisma').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level)
+        character.attribute_skill_judgeintention =  int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Charisma').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Intuition').id).level)
+        character.attribute_skill_lifting =  int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Body').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Strength').id).level)
+        character.attribute_skill_memory =  int(character.characterskill_set.get(skill_id=Skill.objects.get(name='Logic').id).level)+(character.characterskill_set.get(skill_id=Skill.objects.get(name='Willpower').id).level)
+
+        
+
     template = loader.get_template('persomaker/creation.html')
     character = get_object_or_404(Character, pk=pk)
-    print(character)
+    #Faire un filtre qui distingue les skills des attributs
+    attribute_filter = Skill.objects.filter(group='ATTRIBUTE')
+    #set de Skills/attributs
+    attribute_set = character.characterskill_set.filter(skill__in=attribute_filter)
+    skill_set = character.characterskill_set.exclude(skill__in=attribute_filter)
+    #calcul finaux
+    final_calculation(character)
+
+
     try:
         selected_choice = character.characterskill_set.get(pk=request.POST['charskill'])
     except (KeyError, Character.DoesNotExist):
         # Redisplay the question voting form.
+        
         return render(request, 'persomaker/creation.html', {
             'character': character,
+            'attribute_set':attribute_set,
+            'skill_set':skill_set,
             'error_message': "You didn't select a choice.",
         })
     else:
@@ -45,7 +83,7 @@ def creation(request, pk):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         context = {'pk':pk}
-        return HttpResponseRedirect(reverse('persomaker:result', args=(character.id,)))
+        return HttpResponseRedirect(reverse('persomaker:creation', args=(character.id,)))
 
 def echec(request, pk):
     response = "echec %s."
